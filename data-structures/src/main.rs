fd8df70444465cf232eab910aed0c6587ad79469
nabil5352 <nabil.ibn.mahmud@gmail.com>
2021-01-16 23:43:41 +0600

    scope and assignment

--- data-structures/src/main.rs
+++ data-structures/src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     println!("Hello, data structures!");
 
     strings();
+
+    assignment();
 }
 
 fn strings() {
@@ -14,4 +16,15 @@ fn strings() {
     println!("{}", s);
 
     // this scope is over, and s is no longer valid
+}
+
+fn assignment() {
+    let x = 5;
+    let y = x; // both x and y are valid
+    println!("X and Y is: {} {}", x, y);
+
+    let s1 = String::from("hello");
+    //let s2 = s1; // s1 no longer valid
+    let s2 = s1.clone();
+    println!("S1 and S2 is: {} {}", s1, s2);
 }
