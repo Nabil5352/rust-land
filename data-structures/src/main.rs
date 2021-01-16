018a0df5c0e98bdd39a5f8ed6365f52962019fec
nabil5352 <nabil.ibn.mahmud@gmail.com>
2021-01-16 23:55:03 +0600

    scope ownership

--- data-structures/src/main.rs
+++ data-structures/src/main.rs
@@ -4,7 +4,21 @@ fn main() {
     strings();
 
     assignment();
-}
+
+    let s = String::from("world"); // s comes into scope
+    
+    // s's value move into the function. and so is no longer valid here
+    takes_ownership(s);
+
+    let x = 5; // x comes into scope
+
+    // but i32 is Copy, so it's okat to still use x afterward
+    makes_copy(x);
+
+    //println!("{}", s); // s no longer valid
+    // println!("{}", x); // x is valid
+} // here, x goes out of scope, then s.
+// But because s's value was moved, nothing special happens
 
 fn strings() {
     let mut s = String::from("hello");
@@ -28,3 +42,11 @@ fn assignment() {
     let s2 = s1.clone();
     println!("S1 and S2 is: {} {}", s1, s2);
 }
+
+fn takes_ownership(param: String) {
+    println!("{}", param);
+} // Here, param goes out of scope and `drop` is called. The backing memory is freed
+
+fn makes_copy(param: i32) {
+    println!("{}", param);
+} // Here, param goes out of scope. Nothing special happens.
