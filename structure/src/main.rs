e357ad4820d5684d83973956ecbad7db45cc90d8
nabil5352 <nabil.ibn.mahmud@gmail.com>
2021-01-26 00:38:21 +0600

    struct user

--- structure/src/main.rs
+++ structure/src/main.rs
@@ -1,3 +1,21 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Struct!");
+
+    build_user("nabil.ibn.mahmud@pm.me", "nabilahmad");
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        username,
+        email,
+        sign_in_count: 1,
+        active: true
+    }
 }
