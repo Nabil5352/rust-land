882a2c6e33ccf8da3e70a9b898ba6e76784db91c
nabil5352 <nabil.ibn.mahmud@gmail.com>
2021-02-03 22:04:55 +0600

    added ip address

--- enums_pattern/src/main.rs
+++ enums_pattern/src/main.rs
@@ -1,3 +1,38 @@
+enum IpAddrKind {
+    V4, V6
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Enum!");
+
+    enums();
+}
+
+fn enums() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(four);
+    route(six);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1")
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1")
+    };
+    
+}
+
+fn route(ip_type: IpAddrKind) {
+
 }
