9cec3f9ddf38407265cdb7a3479531c621c515d8
Nabil Ahmad <report.nabil@gmail.com>
2021-02-06 22:20:03 +0600

    enums and pattern

--- enums_pattern/src/main.rs
+++ enums_pattern/src/main.rs
@@ -1,38 +1,56 @@
 enum IpAddrKind {
-    V4, V6
+    V4(u8, u8, u8, u8), 
+    V6(String)
 }
 
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska
 }
 
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState)
+}
 
 fn main() {
     println!("Hello, Enum!");
 
     enums();
+
+    let penny = value_in_cents(Coin::Penny);
+    let nickel = value_in_cents(Coin::Nickel);
+    let dime = value_in_cents(Coin::Dime);
+    let quarter1 = value_in_cents(Coin::Quarter(UsState::Alabama));
+    let quarter2 = value_in_cents(Coin::Quarter(UsState::Alaska));
+    println!(
+        "Penny: {}\nNickel: {}\nDime: {},\nAlabama Quarter: {}\nAlaska Quarter: {}", 
+        penny, nickel, dime, quarter1, quarter2);
 }
 
 fn enums() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+    let _four = IpAddrKind::V4;
+    let _six = IpAddrKind::V6;
 
-    route(four);
-    route(six);
+    let _home = IpAddrKind::V4(127, 0, 0, 1);
 
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1")
-    };
-
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1")
-    };
-    
+    let _loopback = IpAddrKind::V6(String::from("::1"));
 }
 
-fn route(ip_type: IpAddrKind) {
-
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
 }
