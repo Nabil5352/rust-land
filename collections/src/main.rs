cce5703fa9e0ca752cbd2e538a1906c9e6461a3f
nabil5352 <nabil.ibn.mahmud@gmail.com>
2021-02-22 23:53:17 +0600

    collections: vector

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -2,11 +2,13 @@ fn main() {
     println!("Hello, collections!");
 
     vectors();
+
+    reading_vector();
 }
 
 fn vectors() {
     // Create
-    let v: Vec<i32> = Vec::new();
+    let _v: Vec<i32> = Vec::new();
 
     let x = vec![1, 2, 3];
 
@@ -21,6 +23,25 @@ fn vectors() {
     let third: &i32 = &x[2];
     let third_alt: Option<&i32> = x.get(2);
 
-    let does_not_exists = &x[100];
+    // let does_not_exists = &x[100];
+    println!("Third: {}\nThird alt: {:?}", third, third_alt);
 
 } // v, x, y goes out of scope and is freed here
+
+fn reading_vector() {
+    let mut v = vec![1, 2, 3, 4, 5];
+
+    let _third: &i32 = &v[2];
+    let _third: Option<&i32> = v.get(2);
+
+    let _first = &v[0];
+    v.push(6);
+
+    for i in &mut v {
+        *i += 50;
+    }
+    
+    for i in &v {
+        println!("{}", i);
+    }
+}
